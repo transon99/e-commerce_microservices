a1daa5f41f9810b2d5f8bab8b6bbad63354396ac
sondev_99 <transon220599@gmail.com>
2023-10-13 02:03:50 +0700

    complete getProductApi

--- product-service/src/main/java/com/eastgate/productservice/service/Impl/ProductServiceImpl.java
+++ product-service/src/main/java/com/eastgate/productservice/service/Impl/ProductServiceImpl.java
@@ -1,5 +1,6 @@
 package com.eastgate.productservice.service.Impl;
 
+import com.eastgate.constants.ResponseStatusCode;
 import com.eastgate.productservice.dto.request.ProductRequest;
 import com.eastgate.productservice.entity.Category;
 import com.eastgate.productservice.entity.Product;
@@ -8,12 +9,14 @@ import com.eastgate.productservice.exceptions.NotFoundException;
 import com.eastgate.productservice.mapper.ProductMapper;
 import com.eastgate.productservice.repository.ProductRepository;
 import com.eastgate.productservice.service.ProductService;
+import com.eastgate.response.PagingData;
 import com.eastgate.response.ResponseDTO;
 import com.eastgate.utils.PaginationUtils;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -47,13 +50,27 @@ public class ProductServiceImpl implements ProductService {
     }
 
     public ResponseDTO getProducts(String searchText, Integer offset, Integer pageSize, String sortStr) {
+        Page<Product> productPage;
         Sort sort = PaginationUtils.buildSort(sortStr);
         Pageable pageable = PageRequest.of(offset, pageSize, sort);
 
         if (StringUtils.isNotEmpty(searchText)){
-            return getResponseSuccess(productMapper.toDto(productRepository.findByNameContainingIgnoreCase(searchText,pageable).toList()),"Successfully!!!");
+            productPage = productRepository.findAll(pageable);
+        }else {
+            productPage = productRepository.findByNameContainingIgnoreCase(searchText,pageable);
         }
-        return getResponseSuccess(productMapper.toDto(productRepository.findAll(pageable).toList()),"Successfully!!!");
+        return ResponseDTO.builder()
+                .data(productMapper.toDto(productPage.toList()))
+                .message("Successfully!!!")
+                .pagingData(PagingData.builder()
+                        .searchText(searchText)
+                        .offset(offset)
+                        .pageSize(pageSize)
+                        .sort(sortStr)
+                        .totalRecord(productPage.getTotalElements())
+                        .build())
+                .responseStatusCode(ResponseStatusCode.OK)
+                .build();
     }
 
     public ResponseDTO updateProduct(Map<String, Object> fields, String id) {
