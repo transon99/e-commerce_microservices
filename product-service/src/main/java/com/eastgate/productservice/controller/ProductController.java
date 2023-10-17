65acde25d119a8a4d41f138e4d264d5bf8ea8770
Son Tran <son.tt@eastgate-software.com>
2023-10-17 11:44:51 +0700

    backup point

--- product-service/src/main/java/com/eastgate/productservice/controller/ProductController.java
+++ product-service/src/main/java/com/eastgate/productservice/controller/ProductController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
-@RequestMapping("/products")
+@RequestMapping("api/v1/products")
 @RestController
 @Slf4j
 @RequiredArgsConstructor
